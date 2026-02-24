37d580abe28c0c1ad72b1ed34e37421dda740ff2
snowfake <63953060+snowflakee-e@users.noreply.github.com>
2026-02-24 09:15:36 +0300

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,1 +1,9 @@
+from main import add_numbers, multiply_numbers
 
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 1) == 0
+
+def test_multiply_numbers():
+    assert multiply_numbers(2, 3) == 6
+    assert multiply_numbers(0, 5) == 0
